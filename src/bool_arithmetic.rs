0dc4269755bb445686854f05baf8da676267a2c7
hhiroki <gyouzakuuka@gmail.com>
2020-06-14 01:51:33 +0900

    implement full adder

--- src/bool_arithmetic.rs
+++ src/bool_arithmetic.rs
@@ -6,10 +6,25 @@ pub fn entry_point() {
     println!("in bool_arithmetic mod entry_point");
 }
 
-fn half_adder(a: u8, b: u8) -> HashMap<&'static str, u8>{
+fn half_adder(a: u8, b: u8) -> HashMap<&'static str, u8> {
     let mut result = HashMap::new();
-    result.insert("carry", bool_logic::and(a, b));
-    result.insert("sum", bool_logic::xor(a, b));
+    let carry: u8 = bool_logic::and(a, b);
+    let sum: u8 = bool_logic::xor(a, b);
+
+    result.insert("carry", carry);
+    result.insert("sum", sum);
+    result
+}
+
+fn full_adder(a: u8, b: u8, c: u8) -> HashMap<&'static str, u8> {
+    let mut result = HashMap::new();
+    let ha_first: HashMap<&'static str, u8> = half_adder(a, b);
+    let ha_second: HashMap<&'static str, u8> = half_adder(c, ha_first["sum"]);
+    let carry: u8 = bool_logic::or(ha_second["carry"], ha_first["carry"]);
+    let sum: u8 = ha_second["sum"];
+
+    result.insert("carry", carry);
+    result.insert("sum", sum);
     result
 }
 
@@ -28,5 +43,26 @@ mod test {
         assert_eq!(1, half_adder(1, 0)["sum"]);
         assert_eq!(1, half_adder(0, 1)["sum"]);
         assert_eq!(0, half_adder(0, 0)["sum"]);
+    }
+
+    #[test]
+    fn full_adder_test() {
+        assert_eq!(0, full_adder(0, 0, 0)["carry"]);
+        assert_eq!(0, full_adder(0, 0, 1)["carry"]);
+        assert_eq!(0, full_adder(0, 1, 0)["carry"]);
+        assert_eq!(1, full_adder(0, 1, 1)["carry"]);
+        assert_eq!(0, full_adder(1, 0, 0)["carry"]);
+        assert_eq!(1, full_adder(1, 0, 1)["carry"]);
+        assert_eq!(1, full_adder(1, 1, 0)["carry"]);
+        assert_eq!(1, full_adder(1, 1, 1)["carry"]);
+
+        assert_eq!(0, full_adder(0, 0, 0)["sum"]);
+        assert_eq!(1, full_adder(0, 0, 1)["sum"]);
+        assert_eq!(1, full_adder(0, 1, 0)["sum"]);
+        assert_eq!(0, full_adder(0, 1, 1)["sum"]);
+        assert_eq!(1, full_adder(1, 0, 0)["sum"]);
+        assert_eq!(0, full_adder(1, 0, 1)["sum"]);
+        assert_eq!(0, full_adder(1, 1, 0)["sum"]);
+        assert_eq!(1, full_adder(1, 1, 1)["sum"]);
     }
 }
